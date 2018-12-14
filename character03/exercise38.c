ce25cec72c7a6631d2adac582f7aa97ebe897b59
Tony <13723786518@163.com>
2018-12-14 14:48:54 +0800

    第三章 习题 例题 2018 12 14，14：48龙四

--- character03/exercise38.c
+++ character03/exercise38.c
@@ -7,13 +7,17 @@ int main()
 	int x1, x2;
 	c1 = getchar();
 	c2 = getchar();
+	x1 = getchar();
+	x2 = getchar();
 	printf("c1=%c\tc2=%c\n", c1, c2);
+	printf("x1=%d\tx2=%d\n", x1, x2);
 	x1 = c1;
 	x2 = c2;
 	printf("x1=%d\tx2=%d\n", x1, x2);
 
-//TODO 要求输出c1,c2的ASCII码，用putchar()还是printf()
+//T/ODO 要求输出c1,c2的ASCII码，用putchar()还是printf()
 	putchar(c1);
+	printf("\n");
 
 	return 0;
 }
